4e7d8b328404b5edc0da3e1d34310c4df212f782
Markus <niiranen.markus@gmail.com>
2021-04-27 14:16:08 +0300

    initial commit

---
+++ lomitus.py
@@ -0,0 +1,33 @@
+from flask import Flask
+import numpy as np
+from qiskit import(QuantumCircuit, execute, Aer)
+from qiskit.visualization import plot_histogram
+
+main_page = """
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Lomittaja</h2>
+<form action="">
+    <button type="submit">Lomita lisää!</button>
+<form>
+ 
+</body>
+</html>
+"""
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    qc = QuantumCircuit(2)
+    # Apply H-gate to the first:
+    qc.h(0)
+    # Apply a CNOT:
+    qc.cx(0,1)
+    print("lomitellaan")
+    return main_page
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
